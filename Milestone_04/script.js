9359b08fbb29692c1c28e58fad87f50c00325f95
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-09-17 18:15:55 +0500

    Update script.js

--- Milestone_04/script.js
+++ Milestone_04/script.js
@@ -1,21 +1,32 @@
 var _a;
-(_a = document.getElementById('Form')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+(_a = document
+    .getElementById("Form")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", function (event) {
+    var _a;
     event.preventDefault();
-    var userName = document.getElementById('name');
-    var userEmail = document.getElementById('email');
-    var userPhone = document.getElementById('phone');
-    var userEducation = document.getElementById('education');
-    var userExperience = document.getElementById('experience');
-    var userSkills = document.getElementById('skills');
-    if (userName && userEmail && userPhone && userEducation && userExperience && userSkills) {
+    var Profilepictureinput = document.getElementById('profilepicture');
+    var userName = document.getElementById("name");
+    var userEmail = document.getElementById("email");
+    var userPhone = document.getElementById("phone");
+    var userEducation = document.getElementById("education");
+    var userExperience = document.getElementById("experience");
+    var userSkills = document.getElementById("skills");
+    if (userName &&
+        Profilepictureinput &&
+        userEmail &&
+        userPhone &&
+        userEducation &&
+        userExperience &&
+        userSkills) {
         var namee = userName.value;
         var email = userEmail.value;
         var phone = userPhone.value;
         var education = userEducation.value;
         var experience = userExperience.value;
         var skills = userSkills.value;
-        var output = "\n <h1>Resume</h1>\n                <p><strong>Name:</strong> <span id=\"edit-name\" class \"editable\"> ".concat(namee, " </span></p>\n                <p><strong>Email:</strong> <span id=\"edit-email\" class \"editable\">  ").concat(email, "</p>\n                <p><strong>Phone:</strong> <span id=\"edit-phone\" class \"editable\">  ").concat(phone, "</p>\n                <h3>Education</h3>\n                <p> <span id=\"edit-education\" class \"editable\"> ").concat(education, "  </p>\n                <h3>Experience</h3>\n                <p> <span id=\"edit-experience\" class \"editable\">").concat(experience, "  </p>\n                <h3>Skills</h3>\n                <p> <span id=\"edit-skills\" class \"editable\">").concat(skills, "  </p>\n\n");
-        var resumeoutputelement = document.getElementById('resume');
+        var Profilepicture = (_a = Profilepictureinput.files) === null || _a === void 0 ? void 0 : _a[0];
+        var profilepictureurl = Profilepicture ? URL.createObjectURL(Profilepicture) : "";
+        var output = "\n <h1>Resume</h1>\n                ".concat(profilepictureurl ? "<img src=\"".concat(profilepictureurl, "\" alt=\"Profile Picture\" class=\"profilepicture\">") : '', "\n    <p><strong>Name:</strong> ").concat(namee, "</p>\n                <p><strong>Email:</strong> <span id=\"edit-email\" class \"editable\">  ").concat(email, "</p>\n                <p><strong>Phone:</strong> <span id=\"edit-phone\" class \"editable\">  ").concat(phone, "</p>\n                <h3>Education</h3>\n                <p> <span id=\"edit-education\" class \"editable\"> ").concat(education, "  </p>\n                <h3>Experience</h3>\n                <p> <span id=\"edit-experience\" class \"editable\">").concat(experience, "  </p>\n                <h3>Skills</h3>\n                <p> <span id=\"edit-skills\" class \"editable\">").concat(skills, "  </p>\n\n");
+        var resumeoutputelement = document.getElementById("resume");
         if (resumeoutputelement) {
             resumeoutputelement.innerHTML = output;
         }
@@ -26,23 +37,23 @@ var _a;
     }
 });
 function makeEditable() {
-    var editable = document.querySelectorAll('.editable');
+    var editable = document.querySelectorAll(".editable");
     editable.forEach(function (element) {
-        element.addEventListener('click', function () {
+        element.addEventListener("click", function () {
             var _a;
             var currentElement = element;
             var currentValue = currentElement.textContent || "";
             if (currentElement.tagName === "p" || currentElement.tagName === "SPAN") {
-                var input_1 = document.createElement('input');
-                input_1.type = 'text';
+                var input_1 = document.createElement("input");
+                input_1.type = "text";
                 input_1.value = currentValue;
-                input_1.classList.add('editing-input');
-                input_1.addEventListener('blur', function () {
+                input_1.classList.add("editing-input");
+                input_1.addEventListener("blur", function () {
                     currentElement.textContent = input_1.value;
-                    currentElement.style.display = 'inline';
+                    currentElement.style.display = "inline";
                     input_1.remove();
                 });
-                currentElement.style.display = 'none';
+                currentElement.style.display = "none";
                 (_a = currentElement.parentNode) === null || _a === void 0 ? void 0 : _a.insertBefore(input_1, currentElement);
             }
         });
